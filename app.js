6cb774d75c2e0ece130dcb9ac5c14199812e356c
YvesConcepcion <118972229+YvesConcepcion@users.noreply.github.com>
2023-05-11 22:27:56 +0800

    Add book constructor in JavaScript

--- app.js
+++ app.js
@@ -1,7 +1,34 @@
+"use strict";
 const openModalButtons = document.querySelectorAll("[data-modal-target]");
 const closeModalButtons = document.querySelectorAll("[data-close-button]");
 const overlay = document.getElementById("overlay");
 
+const form = document.getElementById("form");
+
+let myLibrary = [];
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault(); //Prevents the form from submmitting
+  var title = document.getElementById("title").value;
+  var author = document.getElementById("author").value;
+  var pages = document.getElementById("pages").value;
+  var isRead = document.getElementById("is-read").checked;
+
+  var book = new Book(title, author, pages, isRead);
+
+  myLibrary.push(book);
+});
+
+class Book {
+  constructor(title, author, pages, isRead) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.isRead = isRead;
+  }
+}
+
+//Modals
 openModalButtons.forEach((button) => {
   button.addEventListener("click", () => {
     const modal = document.querySelector(button.dataset.modalTarget);
@@ -34,3 +61,6 @@ overlay.addEventListener("click", () => {
     closeModal(modal);
   });
 });
+
+var divBook = document.createElement("div");
+divBook.setAttribute("id", "new-book");
